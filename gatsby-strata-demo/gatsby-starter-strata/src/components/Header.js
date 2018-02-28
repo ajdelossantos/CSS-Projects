be4dd0763a2f4ec3966f66eb795066f7962743a4
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-02-28 04:15:32 -0800

    Initial gatsby demo commit

---
+++ gatsby-strata-demo/gatsby-starter-strata/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Footer from './Footer';
+import avatar from '../assets/images/avatar_1.jpg';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header id="header">
+                <div className="inner">
+                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
+                    <h1><strong>Angelina Nwin</strong></h1> 
+                    <h1>artist</h1>
+                    <h1>yogi</h1>
+                    <h1>expert in smiles</h1>
+                </div>
+                <Footer />
+            </header>
+        );
+    }
+}
+
+export default Header
